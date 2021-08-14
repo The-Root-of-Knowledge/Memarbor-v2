17ec2cf0ec99e6c822533185134927a04a720fcb
Brandon Bowers <coderbrandonbowers@DESKTOP-4HEFAVI.localdomain>
2021-08-14 13:39:22 -0500

    Add requires to card router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+//list of routers
 const cardRouters = require("./routes/cardRouter");
 const authRouters = require("./routes/authRouter");
 
