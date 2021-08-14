033c75aff806a7285807dd167aa3a21c56a675a2
Matt von der Lippe <mvdlippe@gmail.com>
2021-08-14 11:52:16 -0400

    Boilerplate ready

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/build/',
+    proxy: {
+      '/': 'http://localhost:3000',
+    }
+  }
+}
