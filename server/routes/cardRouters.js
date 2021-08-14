01d051a9b0eb2788496ba2410bd14ac79b1346b1
Brandon Bowers <coderbrandonbowers@DESKTOP-4HEFAVI.localdomain>
2021-08-14 13:56:52 -0500

    Card Router Logic Implemented

--- server/routes/cardRouters.js
+++ server/routes/cardRouters.js
@@ -4,4 +4,19 @@ const cardController = require("../controllers/cardController");
 
 const router = express.Router();
 
-router.get("");
+//route to get a set of cards
+router.get("/getSet", cardController.getSet, (req, res) =>
+  res.status(200).send(res.locals.set)
+);
+
+//route to create new set
+router.post("/createSet", cardController.createSet, (req, res) =>
+  res.status(200).send(res.locals.newSet)
+);
+
+//route to create new card
+router.post("/createCard", cardController.createCard, (req, res) =>
+  res.status(200).send(res.locals.newCard)
+);
+
+//getSet request needs to happen after client switches from card creation mode to practice mode
