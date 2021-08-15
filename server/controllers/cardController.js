10438a4221f93031d111f07ffc7dbfdffea3f8c1
Monchichi <monchichi@Monchichis-Air.lan>
2021-08-15 11:23:22 -0400

    All get and post functionality for cards and sets complete

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -4,6 +4,7 @@ const db = require("../cardModel.js");
 const cardController = {};
 
 cardController.getSet = (req, res, next) => {
+  //_id comes from front end based on what set user clicked
   const setId = [req.body._id];
   //console.log(setId, "Should be 1");
   const queryString = `SELECT s.*, c.* FROM sets s LEFT OUTER JOIN cards c ON c.set_id = $1;`;
@@ -27,17 +28,65 @@ cardController.createSet = (req, res, next) => {
     //Will need to pass setname and private = 0 to db
     db.query(queryString, setName)
     .then((data) => {
-    res.locals.newSet = data;
-    console.log(res.locals.newSet)
+    res.locals.newSetName = setName;
+    //console.log(res.locals.newSetName)
     return next();
     })
     .catch((err) => {
-      console.log(err);
+      console.log('error in create set middleware', err);
       return next(err);
     });
     
 };
 
-cardController.createCard = (req, res, next) => {};
+cardController.getSetByName = (req, res, next) => {
+  const setName = res.locals.newSetName //is an array already
+  const queryString = `SELECT sets._id FROM sets WHERE sets.setname = $1;`
+
+  db.query(queryString, setName)
+  .then((data) => {
+  res.locals.setId = data.rows;
+  return next();
+  })
+  .catch((err) => {
+    console.log('error in getSetByName middleware', err);
+    return next(err);
+  });
+}
+
+cardController.createCard = (req, res, next) => {
+  //USER SENDS: question, imageurl, answer, FOR NOW HARDCODE TO 1: difficulty, SENT FROM FRONT-END BASED ON SET USER IS IN: set_id
+  const cardData = [
+    req.body.question,
+    req.body.imageurl,
+    req.body.answer,
+    1,
+    req.body.set_id
+  ];
+
+  const queryString = `INSERT INTO cards (question, imageurl, answer, difficulty, set_id) VALUES ($1, $2, $3, $4, $5);`
+
+  db.query(queryString, cardData)
+  .then(() => {
+    return next();
+  })
+  .catch((err) => {
+    console.log('error in create card middleware', err);
+    return next(err);
+  });
+};
+
+cardController.getAllSets = (req, res, next) => {
+  const queryString = `SELECT * FROM sets;`
+  db.query(queryString)
+  .then((data) => {
+    res.locals.allSets = data.rows;
+    return next();
+  })
+  .catch((err) => {
+    console.log('error in get all sets middleware', err);
+    return next(err);
+  });
+}
 
 module.exports = cardController;
