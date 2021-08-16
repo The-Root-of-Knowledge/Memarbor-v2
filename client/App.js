4b78c3fa71bcf3b2b0874e18919344b2330c1bab
Matt von der Lippe <mvdlippe@gmail.com>
2021-08-16 16:30:42 -0400

    Added a login component and some state variables to go with it

--- client/App.js
+++ client/App.js
@@ -9,10 +9,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: null
+      mode: null,
+      loggedIn: null,
+      username: '',
     };
 
     this.changeMode = this.changeMode.bind(this);
+    this.logInUser = this.logInUser.bind(this);
   }
 
   changeMode (mode) {
@@ -20,10 +23,22 @@ class App extends Component {
     this.setState(this.state);
   }
 
+  createNewUser (credentials) {
+    console.log(credentials);
+  }
+
+  logInUser (credentials) {
+    console.log(credentials);
+    // Make a post request to the server and attempt to log in
+
+    // Update this.state.loggedIn and this.state.username if login was successful
+
+  }
+
   modeDisplay () {
     if (this.state.mode === 'practice') return <Practice />;
     else if (this.state.mode === 'create') return <Create />;
-    else if (this.state.mode === 'login') return <LogIn />;
+    else if (this.state.mode === 'login') return <LogIn logInUser={this.logInUser}/>;
     else if (this.state.mode === 'signup') return <SignUp />;
     else return <div />;
   }
--- client/components/login.js
+++ client/components/login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles({
-
+  loginBox: {
+    margin: 40,
+  },
+  spacer: {
+    margin: 10,
+  },
 });
 
 export default function LogIn(props) {
-  return <h3>Login page goes here</h3>
+  const classes = useStyles();
+
+  const gatherCredentials = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    return [username, password];
+  }
+
+  return (
+    <div className={classes.loginBox}>
+      <TextField id="username" className={classes.spacer} variant="outlined" label="Username"></TextField>
+      <TextField id="password" className={classes.spacer} variant="outlined" label="Password" type="password"></TextField>
+      <br></br>
+      <Button className={classes.spacer} size="large" variant="contained" color="primary" onClick={() => props.logInUser(gatherCredentials())}>Log In</Button>
+    </div>
+  )
 }
