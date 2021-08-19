4166ca5a2dc571d7926e4d473755ba8f771fe386
Brandon Bowers <coderbrandonbowers@DESKTOP-4HEFAVI.localdomain>
2021-08-19 15:32:44 -0500

    signup credentials length restrictions setup

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles({
   loginBox: {
@@ -16,24 +16,41 @@ export default function SignUp(props) {
   const classes = useStyles();
 
   const gatherCredentials = () => {
-    const username = document.getElementById('newUsername').value;
-    const password = document.getElementById('newPassword').value;
+    let username = document.getElementById("newUsername").value;
+    const password = document.getElementById("newPassword").value;
     return [username, password];
-  }
+  };
 
   const signupMessage = () => {
-    if (props.loggedIn === true) return <p>Welcome! Let's make some flashcards!</p>
-    else if (props.loggedIn === false) return <p>There was an issue creating your account.</p>
-    else return <div></div>
-  }
+    return <p>{props.signUpResponse}</p>;
+  };
 
   return (
     <div className={classes.loginBox}>
-      <TextField id="newUsername" className={classes.spacer} variant="outlined" label="Username"></TextField>
-      <TextField id="newPassword" className={classes.spacer} variant="outlined" label="Password" type="password"></TextField>
+      <TextField
+        id="newUsername"
+        className={classes.spacer}
+        variant="outlined"
+        label="Username"
+      ></TextField>
+      <TextField
+        id="newPassword"
+        className={classes.spacer}
+        variant="outlined"
+        label="Password"
+        type="password"
+      ></TextField>
       <br></br>
-      <Button className={classes.spacer} size="large" variant="contained" color="primary" onClick={() => props.createNewUser(gatherCredentials())}>Create my account</Button>
+      <Button
+        className={classes.spacer}
+        size="large"
+        variant="contained"
+        color="primary"
+        onClick={() => props.createNewUser(gatherCredentials())}
+      >
+        Create my account
+      </Button>
       {signupMessage()}
     </div>
-  )
+  );
 }
